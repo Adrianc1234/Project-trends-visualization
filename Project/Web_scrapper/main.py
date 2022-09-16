00a10e91103a808e81bde022d0c9515f952c87ed
艾德里安 <adrianrobertocarmona49@gmail.com>
2022-09-16 01:21:49 -0500

    version - 3.0

---
+++ Project/Web_scrapper/main.py
@@ -0,0 +1,68 @@
+from scrapper import Instagram_Scrap
+import json
+
+
+#Logging in 
+web = Instagram_Scrap()
+web.login()
+
+
+#Go to the desired profile
+username = web.go_to_profile()
+
+# Extract number of posts, followers and followings
+info, name, description,verified, profession_final = web.extract_info_profile()
+
+print('Description:', description)
+
+# Extract general info of the profile: photos, number of likes, date
+number_likes_list, dates, photos, links_post = web.extract_info_post()
+
+#It closes the browser
+web.close()
+
+
+# Saving the information
+
+user = dict()
+
+
+user['username'] = username
+user['number_post'] = info[0]
+user['number_followers'] = info[1]
+user['number_followings'] = info[2]
+user['real_name'] = name
+user['description'] = description
+user['verified'] = verified
+user['profession'] = profession_final
+
+
+
+user_posts = dict()
+print(len(number_likes_list) , len(photos))
+
+for i in range(len(photos)):
+    user_posts[f'user_post{i}'] = {}
+
+    user_posts[f'user_post{i}']['id'] = i
+    user_posts[f'user_post{i}']['likes'] = (number_likes_list[i])
+    user_posts[f'user_post{i}']['date'] = dates[i]
+    user_posts[f'user_post{i}']['photo'] = photos[i]
+    user_posts[f'user_post{i}']['url_post'] = links_post[i]
+
+user['posts_info'] = user_posts
+print(user)
+    
+
+with open("scrapper.json", "w") as outfile:
+    json.dump(user, outfile)
+
+
+
+
+
+
+
+
+
+
